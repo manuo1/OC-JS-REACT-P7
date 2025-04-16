e52636fd73123fe4f5c3716cad2c30f3a9f991ec
manuo1 <manuo1@free.fr>
2025-04-16 18:20:47 +0200

    feat(filetr-dropdown) Add selected item from available part in selected part dropdown

--- src/js/components/filterDropdown/FilterDropdownsManager.js
+++ src/js/components/filterDropdown/FilterDropdownsManager.js
@@ -12,82 +12,62 @@ class FilterDropdownsManager {
     return FILTER_TYPES.map((filterTypes) => createFilterDropdown(filterTypes));
   }
 
-  display() {
+  addToDOM() {
     const filterDropdownSection = document.getElementById("filter-dropdowns-section");
     this.dropdowns.forEach((dropdown) => {
       filterDropdownSection.appendChild(dropdown);
     });
   }
 
-  setupToggles() {
-    const keys = FILTER_TYPES.map((item) => item.key);
-    keys.forEach((key) => {
-      const filterButton = document.getElementById(`${key}-filter-button`);
-      const dropdownWrapper = document.getElementById(`${key}-dropdown-wrapper`);
-
-      filterButton.addEventListener("click", () => {
-        dropdownWrapper.classList.toggle("hidden");
-        filterButton.classList.toggle("rounded-b-[11px]");
-        const ariaExpanded = filterButton.getAttribute("aria-expanded") === "true";
-        filterButton.setAttribute("aria-expanded", !ariaExpanded);
-      });
-    });
+  updateInnerElements(searchState) {
+    this.clearLists();
+    this.addAvailableItems(searchState.getFilteredRecipesAppliances(), APPLIANCE);
+    this.addAvailableItems(searchState.getFilteredRecipesIngredients(), INGREDIENTS);
+    this.addAvailableItems(searchState.getFilteredRecipesUstensils(), USTENSILS);
+    this.addSelectedItems(searchState.selectedAppliances, APPLIANCE);
+    this.addSelectedItems(searchState.selectedIngredients, INGREDIENTS);
+    this.addSelectedItems(searchState.selectedUstensils, USTENSILS);
   }
 
-  updateItems(searchState) {
-    this.clearAvailableLists();
-
-    this.addItems(searchState.getFilteredRecipesAppliances(), APPLIANCE, DROPDOWN_FILTER_SECTION.available);
-    this.addItems(searchState.getFilteredRecipesIngredients(), INGREDIENTS, DROPDOWN_FILTER_SECTION.available);
-    this.addItems(searchState.getFilteredRecipesUstensils(), USTENSILS, DROPDOWN_FILTER_SECTION.available);
-    this.addItems(searchState.selectedIngredients, APPLIANCE, DROPDOWN_FILTER_SECTION.selected);
-    this.addItems(searchState.selectedUstensils, INGREDIENTS, DROPDOWN_FILTER_SECTION.selected);
-    this.addItems(searchState.selectedAppliances, USTENSILS, DROPDOWN_FILTER_SECTION.selected);
-  }
-
-  addItems(labelList, filterType, sectionList) {
-    labelList.forEach((value) => {
-      this.addItem(capitalize(value), filterType, sectionList);
-    });
-  }
-
-  addItem(label, filterType, sectionList) {
-    const id = `${filterType.key}-${sectionList}`;
+  addSelectedItems(labelSet, filterType) {
+    const id = `${filterType.key}-${DROPDOWN_FILTER_SECTION.selected}`;
     const container = document.getElementById(id);
     if (!container) {
       console.error(`${id} Does not exits`);
       return;
     }
-    switch (sectionList) {
-      case DROPDOWN_FILTER_SECTION.selected:
-        container.appendChild(createSelectedItem(label));
-        break;
 
-      case DROPDOWN_FILTER_SECTION.available:
-        container.appendChild(createAvailableItem(label));
-        break;
-
-      default:
-        console.error(`${id} Does not exits`);
-        break;
-    }
+    labelSet.forEach((label) => {
+      container.appendChild(createSelectedItem(capitalize(label)));
+    });
   }
 
-  clearList(filterType, sectionList) {
-    const id = `${filterType.key}-${sectionList}`;
+  addAvailableItems(labelSet, filterType) {
+    const id = `${filterType.key}-${DROPDOWN_FILTER_SECTION.available}`;
     const container = document.getElementById(id);
-
     if (!container) {
-      console.error(`${id} does not exist`);
+      console.error(`${id} Does not exits`);
       return;
     }
 
-    container.innerHTML = "";
+    labelSet.forEach((label) => {
+      container.appendChild(createAvailableItem(capitalize(label)));
+    });
   }
 
-  clearAvailableLists() {
+  clearLists() {
     FILTER_TYPES.forEach((filterType) => {
-      this.clearList(filterType, DROPDOWN_FILTER_SECTION.available);
+      Object.values(DROPDOWN_FILTER_SECTION).forEach((sectionList) => {
+        const id = `${filterType.key}-${sectionList}`;
+        const container = document.getElementById(id);
+
+        if (!container) {
+          console.error(`${id} does not exist`);
+          return;
+        }
+
+        container.innerHTML = "";
+      });
     });
   }
 }
