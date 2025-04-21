f003319998e3e0b5be2479c5748cc11c319383ac
manuo1 <manuo1@free.fr>
2025-04-21 16:42:47 +0200

    refacto(utils.js) Add normalizeSet function

--- src/js/utils/utils.js
+++ src/js/utils/utils.js
@@ -2,10 +2,15 @@ function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
-const normalize = (str) =>
-  str
+function normalize(str) {
+  return str
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "")
     .toLowerCase();
+}
 
-export { capitalize, normalize };
+function normalizeSet(inputSet) {
+  return new Set([...inputSet].map(normalize));
+}
+
+export { capitalize, normalize, normalizeSet };
