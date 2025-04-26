94b55d53d77ec2af262d2c100142017b7c57d10d
manuo1 <manuo1@free.fr>
2025-04-26 17:21:30 +0200

    feat(search): add main search algorithm

--- src/js/services/mainSearch.js
+++ src/js/services/mainSearch.js
@@ -6,35 +6,12 @@ function refreshRecipesByMainSearch(searchText, recipeList) {
   }
 
   const normalizedSearch = normalize(searchText);
-  const results = [];
-
-  for (let i = 0; i < recipeList.length; i++) {
-    const recipe = recipeList[i];
-
-    // search in recipe name
-    if (normalize(recipe.name).includes(normalizedSearch)) {
-      results.push(recipe);
-      continue;
-    }
-
-    // search in recipe description
-    if (normalize(recipe.description).includes(normalizedSearch)) {
-      results.push(recipe);
-      continue;
-    }
-
-    // search in recipe ingredients
-    let j = 0;
-    while (j < recipe.ingredients.length) {
-      if (normalize(recipe.ingredients[j].ingredient).includes(normalizedSearch)) {
-        results.push(recipe);
-        break;
-      }
-      j++;
-    }
-  }
-
-  return results;
+  return recipeList.filter(
+    (recipe) =>
+      normalize(recipe.name).includes(normalizedSearch) ||
+      normalize(recipe.description).includes(normalizedSearch) ||
+      recipe.ingredients.some((ingredient) => normalize(ingredient.ingredient).includes(normalizedSearch))
+  );
 }
 
 export { refreshRecipesByMainSearch };
