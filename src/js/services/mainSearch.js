e5df73769e6b8692c392c7e4d125718a0df6e2d9
manuo1 <manuo1@free.fr>
2025-04-26 17:18:34 +0200

    feat(search): add main search algorithm

--- src/js/services/mainSearch.js
+++ src/js/services/mainSearch.js
@@ -1,10 +1,40 @@
+import { normalize } from "../utils/utils.js";
+
 function refreshRecipesByMainSearch(searchText, recipeList) {
   if (searchText === "" || searchText.length < 3) {
     return recipeList;
-  } else {
-    // search algo soon :)
-    //return recipeList.slice(5, 15);
-    return [];
   }
+
+  const normalizedSearch = normalize(searchText);
+  const results = [];
+
+  for (let i = 0; i < recipeList.length; i++) {
+    const recipe = recipeList[i];
+
+    // search in recipe name
+    if (normalize(recipe.name).includes(normalizedSearch)) {
+      results.push(recipe);
+      continue;
+    }
+
+    // search in recipe description
+    if (normalize(recipe.description).includes(normalizedSearch)) {
+      results.push(recipe);
+      continue;
+    }
+
+    // search in recipe ingredients
+    let j = 0;
+    while (j < recipe.ingredients.length) {
+      if (normalize(recipe.ingredients[j].ingredient).includes(normalizedSearch)) {
+        results.push(recipe);
+        break;
+      }
+      j++;
+    }
+  }
+
+  return results;
 }
+
 export { refreshRecipesByMainSearch };
